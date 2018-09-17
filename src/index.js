14ed456e45f83b4d6e1c533503a019236ab91376
mike80by@gmail.com <your_email@whatever.com>
2018-09-17 20:19:24 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var brOpen = [];
+  for (let i = 0; i < str.length; i++) {
+  	for (let j = 0; j < bracketsConfig.length; j++) {
+  		if (bracketsConfig[j][0] === brOpen[brOpen.length - 1] && str[i] === bracketsConfig[j][1]) {
+  			brOpen.pop();
+  		} else if (str[i] === bracketsConfig[j][0]) {
+  			brOpen.push(str[i]);
+  		}  
+  	}
+  }
+  if (str.length % 2 !== 0) {
+  	return false;
+  } else return (brOpen.length)? false: true;
 }
